2eca8277c4944b364c84d3f3aea3dadfbf2c31dc
Angelovski <ang@who.com>
2024-10-29 00:33:48 +0100

    Styling changes for login and register pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,6 +9,8 @@ import { Eye, EyeOff } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
 
 import {
   Form,
@@ -41,98 +43,87 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-md space-y-8">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
-          Sign in to your account
-        </h2>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="mt-8 space-y-6"
-          >
-            <div className="rounded-md shadow-sm space-y-4">
-              <div>
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email address</FormLabel>
+    <Card className="p-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px]">
+      <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+        Log in
+      </h2>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="mt-8 space-y-6"
+        >
+          <div className="rounded-md shadow-sm space-y-4">
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email address</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="mt-1"
+                        placeholder="john@doe.com"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New Password</FormLabel>
+                    <div className="relative">
                       <FormControl>
                         <Input
                           {...field}
-                          type="email"
-                          autoComplete="email"
+                          type={showPassword ? "text" : "password"}
+                          autoComplete="current-password"
                           required
                           className="mt-1"
-                          placeholder="john@doe.com"
+                          placeholder="A secure password"
                           disabled={isPending}
                         />
                       </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>New Password</FormLabel>
-                      <div className="relative">
-                        <FormControl>
-                          <Input
-                            {...field}
-                            type={showPassword ? "text" : "password"}
-                            autoComplete="current-password"
-                            required
-                            className="mt-1"
-                            placeholder="A secure password"
-                            disabled={isPending}
-                          />
-                        </FormControl>
-                        <button
-                          type="button"
-                          className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {showPassword ? (
-                            <EyeOff className="h-4 w-4 text-gray-500" />
-                          ) : (
-                            <Eye className="h-4 w-4 text-gray-500" />
-                          )}
-                        </button>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+                      <button
+                        type="button"
+                        className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4 text-gray-500" />
+                        ) : (
+                          <Eye className="h-4 w-4 text-gray-500" />
+                        )}
+                      </button>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
+          </div>
 
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <Checkbox id="remember-me" />
-                <Label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900 dark:text-gray-100"
-                >
-                  Remember me
-                </Label>
-              </div>
+          <div>
+            <Button type="submit" className="w-full">
+              {isPending ? "Loading..." : "Log in"}
+            </Button>
+            <div className="mt-4 text-center text-sm text-gray-500">
+              Don't have an account? <Link href="/register">Register</Link>
             </div>
-
-            <div>
-              <Button type="submit" className="w-full">
-                {isPending ? "Loading..." : "Log in"}
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </div>
-    </div>
+          </div>
+        </form>
+      </Form>
+    </Card>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -23,6 +23,8 @@ import { register } from "@/actions";
 import { RegisterSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { useRouter } from "next/navigation";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function RegisterPage() {
   const [showPassword, setShowPassword] = useState(false);
@@ -48,120 +50,109 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-md space-y-8">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
-          Create a new account
-        </h2>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="mt-8 space-y-6"
-          >
-            <div className="rounded-md shadow-sm space-y-4">
-              <div>
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="text"
-                          required
-                          className="mt-1"
-                          placeholder="JohnDoe"
-                          disabled={isPending}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email address</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="email"
-                          autoComplete="email"
-                          required
-                          className="mt-1"
-                          placeholder="john@doe.com"
-                          disabled={isPending}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div>
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>New Password</FormLabel>
-                      <div className="relative">
-                        <FormControl>
-                          <Input
-                            {...field}
-                            type={showPassword ? "text" : "password"}
-                            autoComplete="current-password"
-                            required
-                            className="mt-1"
-                            placeholder="A secure password"
-                            disabled={isPending}
-                          />
-                        </FormControl>
-                        <button
-                          type="button"
-                          className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {showPassword ? (
-                            <EyeOff className="h-4 w-4 text-gray-500" />
-                          ) : (
-                            <Eye className="h-4 w-4 text-gray-500" />
-                          )}
-                        </button>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <Checkbox id="remember-me" />
-                <Label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900 dark:text-gray-100"
-                >
-                  Remember me
-                </Label>
-              </div>
-            </div>
-
+    <Card className="max-w-md p-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] lg:w-auto">
+      <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+        Register
+      </h2>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="mt-8 space-y-6"
+        >
+          <div className="rounded-md shadow-sm space-y-4">
             <div>
-              <Button type="submit" className="w-full">
-                {isPending ? "Loading..." : "Register"}
-              </Button>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        required
+                        className="mt-1"
+                        placeholder="JohnDoe"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-          </form>
-        </Form>
-      </div>
-    </div>
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email address</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="mt-1"
+                        placeholder="john@doe.com"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New Password</FormLabel>
+                    <div className="relative">
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type={showPassword ? "text" : "password"}
+                          autoComplete="current-password"
+                          required
+                          className="mt-1"
+                          placeholder="A secure password"
+                          disabled={isPending}
+                        />
+                      </FormControl>
+                      <button
+                        type="button"
+                        className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4 text-gray-500" />
+                        ) : (
+                          <Eye className="h-4 w-4 text-gray-500" />
+                        )}
+                      </button>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+
+          <div>
+            <Button type="submit" className="w-full">
+              {isPending ? "Loading..." : "Register"}
+            </Button>
+            <div className="mt-4 text-center text-sm text-gray-500">
+              Already have an account? <Link href="/login">Log in</Link>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </Card>
   );
 }
